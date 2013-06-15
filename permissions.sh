ae426765659b2e4c3432c162ba90f8cba0b42b23
deepak <deeps.adhi@gmail.com>
2013-06-15 21:11:31 +0545

    username fixed

--- permissions.sh
+++ permissions.sh
@@ -3,12 +3,12 @@
 
 # permissions for directories
 sudo find . -type d -exec chown nobody {} +
-sudo find . -type d -exec chgrp deepak {} +
+sudo find . -type d -exec chgrp $USER {} +
 sudo find . -type d -exec chmod 570 {} +
 
 #permissions for files
 sudo find . -type f -exec chown nobody {} +
-sudo find . -type f -exec chgrp deepak {} +
+sudo find . -type f -exec chgrp $USER {} +
 sudo find . -type f -exec chmod 470 {} +
 
 #permisssion for logs directory
