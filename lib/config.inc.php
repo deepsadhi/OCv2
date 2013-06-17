f9a6e846bb89239848874c1e82bfd025d7734900
deepak <deeps.adhi@gmail.com>
2013-06-17 20:35:45 +0545

    DB_USER DB_PASS changed

--- lib/config.inc.php
+++ lib/config.inc.php
@@ -6,6 +6,6 @@
  */
 
 defined('DB_HOST') ? null : define('DB_HOST', 'localhost');
-defined('DB_USER') ? null : define('DB_USER', 'admin');
-defined('DB_PASS') ? null : define('DB_PASS', 'logadmin');
+defined('DB_USER') ? null : define('DB_USER', '');
+defined('DB_PASS') ? null : define('DB_PASS', '');
 defined('DB_NAME') ? null : define('DB_NAME', 'oil_price');
