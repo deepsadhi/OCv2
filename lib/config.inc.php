e6a7bd73f1c6f33049726e73d18cbf77cfeced0e
Kshitiz Tiwari <tiwarikshitiz@gmail.com>
2013-06-21 21:59:19 +0545

    Design Work Started

--- lib/config.inc.php
+++ lib/config.inc.php
@@ -6,6 +6,6 @@
  */
 
 defined('DB_HOST') ? null : define('DB_HOST', 'localhost');
-defined('DB_USER') ? null : define('DB_USER', '');
+defined('DB_USER') ? null : define('DB_USER', 'root');
 defined('DB_PASS') ? null : define('DB_PASS', '');
 defined('DB_NAME') ? null : define('DB_NAME', 'oil_price');
